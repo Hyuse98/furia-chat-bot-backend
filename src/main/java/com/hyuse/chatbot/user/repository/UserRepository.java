e29a337f7ecc5255a52f64b64894a7288c4418a1
Franklin <franklinpereira98@hotmail.com>
2025-04-30 20:16:06 -0300

    Change decorator

--- src/main/java/com/hyuse/chatbot/user/repository/UserRepository.java
+++ src/main/java/com/hyuse/chatbot/user/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.hyuse.chatbot.user.repository;
 import com.hyuse.chatbot.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@EnableJpaRepositories
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
