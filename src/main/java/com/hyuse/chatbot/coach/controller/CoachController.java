87ff74ecfff7d8ebc95b11aed1935adcbe1bfdfa
Franklin <franklinpereira98@hotmail.com>
2025-05-15 15:23:24 -0300

    Update Coach Controller

--- src/main/java/com/hyuse/chatbot/coach/controller/CoachController.java
+++ src/main/java/com/hyuse/chatbot/coach/controller/CoachController.java
@@ -1,4 +1,75 @@
 package com.hyuse.chatbot.coach.controller;
 
+import com.hyuse.chatbot.coach.model.dto.CoachDTO;
+import com.hyuse.chatbot.coach.service.CoachService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/coaches")
 public class CoachController {
+
+    private final CoachService coachService;
+
+    @Autowired
+    public CoachController(CoachService coachService) {
+        this.coachService = coachService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CoachDTO> createCoach(@Valid @RequestBody CoachDTO coachDTO) {
+        try {
+            CoachDTO createdCoach = coachService.create(coachDTO);
+            return new ResponseEntity<>(createdCoach, HttpStatus.CREATED);
+        } catch (EntityExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CoachDTO> getCoachById(@PathVariable Long id) {
+        try {
+            CoachDTO coachDTO = coachService.getById(id);
+            return ResponseEntity.ok(coachDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CoachDTO>> getAllCoaches() {
+        List<CoachDTO> coachDTOs = coachService.getAll();
+        return ResponseEntity.ok(coachDTOs);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CoachDTO> updateCoach(@PathVariable Long id, @Valid @RequestBody CoachDTO coachDTO) {
+        try {
+            CoachDTO updatedCoach = coachService.update(coachDTO, id);
+            return ResponseEntity.ok(updatedCoach);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCoach(@PathVariable Long id) {
+        try {
+            coachService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
